9081a0541ddd932e32a5cda737b3f1a81bf352a2
강동욱 <ehddnr8563@gmail.com>
2025-03-27 00:37:20 +0900

    chore: 배너 mock 데이터 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 import { Global } from '@emotion/react'
 
-import globalStyles from '@/styles/globalStyles'
-import Layout from '@/components/shared/Layout'
+import globalStyles from '@styles/globalStyles'
+import Layout from '@shared/Layout'
 
 /**
  * 모든 컴포넌트들이 거치는 레이아웃 컴포넌트
