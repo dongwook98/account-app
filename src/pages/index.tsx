77bf4edecc285d39c37c64cb671bb8e981b74015
강동욱 <ehddnr8563@gmail.com>
2025-03-27 04:15:20 +0900

    feat: 홈페이지 - 신용점수 섹션 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,23 @@ import dynamic from 'next/dynamic'
 
 import Account from '@/components/home/Account'
 import { BannerSkeleton } from '@components/home/EventBanners'
+import { CreditScoreSkeleton } from '@components/home/CreditScore'
+import { CardListSkeleton } from '@/components/home/CardList'
+import Spacing from '@/components/shared/Spacing'
 
 // dynamic = React.lazy + Suspense 합친거
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   loading: () => <BannerSkeleton />,
+  ssr: false,
+})
+
+const CreditScore = dynamic(() => import('@components/home/CreditScore'), {
+  loading: () => <CreditScoreSkeleton />,
+  ssr: false,
+})
+
+const CardList = dynamic(() => import('@components/home/CardList'), {
+  loading: () => <CardListSkeleton />,
   ssr: false,
 })
 
@@ -14,6 +27,10 @@ export default function Home() {
     <>
       <EventBanners />
       <Account />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CreditScore />
+      <Spacing size={8} backgroundColor="gray100" />
+      <CardList />
     </>
   )
 }
